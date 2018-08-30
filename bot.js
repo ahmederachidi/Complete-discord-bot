87a2dc5a30488ffaf421aabce6e6d7e6757417d6
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-08-31 00:15:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1127,7 +1127,7 @@ client.on('message', message => {
 
 client.on("message", message => {
               var args = message.content.substring(prefix.length).split(" ");
-              if (message.content.startsWith(prefix + "مسح")) {
+              if (message.content.startsWith("مسح")) {
                   if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
      if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
           var msg;
